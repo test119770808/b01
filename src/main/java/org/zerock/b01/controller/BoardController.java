71ca250c36879498cf56fec33583048aebe97199
test119770808 <test119770808@gmail.com>
2024-04-30 10:30:14 +0900

    240430 1교시

--- src/main/java/org/zerock/b01/controller/BoardController.java
+++ src/main/java/org/zerock/b01/controller/BoardController.java
@@ -1,11 +1,15 @@
 package org.zerock.b01.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.b01.dto.BoardDTO;
 import org.zerock.b01.dto.PageRequestDTO;
 import org.zerock.b01.dto.PageResponseDTO;
@@ -28,6 +32,35 @@ public class BoardController {
 
         model.addAttribute("responseDTO",responseDTO);
 
+    }
+
+    @GetMapping("/register")
+    public void registerGet() {
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(
+            @Valid BoardDTO boardDTO,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes) {
+
+        log.info("board POST register.......");
+
+        // 값 검증 이후 확인....
+        if(bindingResult.hasErrors()) {   //검증시 에러 있는 경우...
+            log.info("has Errors..... ");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/board/register";
+        }
+
+        // 등록 작업
+        log.info(boardDTO);
+
+        Long bno = boardService.register(boardDTO);
+        redirectAttributes.addFlashAttribute("result", bno);
+
+        return "redirect:/board/list";
     }
 
 }
--- src/main/java/org/zerock/b01/dto/BoardDTO.java
+++ src/main/java/org/zerock/b01/dto/BoardDTO.java
@@ -1,5 +1,7 @@
 package org.zerock.b01.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,8 +15,15 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class BoardDTO {
     private Long bno;
+
+    @NotEmpty
+    @Size(min = 3, max = 100)
     private String title;
+
+    @NotEmpty
     private String content;
+
+    @NotEmpty
     private String writer;
     private LocalDateTime regDate;
     private LocalDateTime modDate;
