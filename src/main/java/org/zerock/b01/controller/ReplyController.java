c8ac6d9832be32fcc1a69efab9ae6aeb11f866e2
test119770808 <test119770808@gmail.com>
2024-05-02 10:20:31 +0900

    240502 1교시

--- src/main/java/org/zerock/b01/controller/ReplyController.java
+++ src/main/java/org/zerock/b01/controller/ReplyController.java
@@ -3,24 +3,29 @@ package org.zerock.b01.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.zerock.b01.dto.PageRequestDTO;
+import org.zerock.b01.dto.PageResponseDTO;
 import org.zerock.b01.dto.ReplyDTO;
+import org.zerock.b01.service.ReplyService;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/replies")
 @Log4j2
+@RequiredArgsConstructor
 public class ReplyController {
-    
+
+    private final ReplyService replyService;  //의존성 주입
+
     @Operation(summary = "Replies Post - Post방식으로 댓글 등록")
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
     public Map<String, Long> register(
@@ -32,9 +37,29 @@ public class ReplyController {
             throw new BindException(bindingResult);
         }
 
-        Map<String, Long> resultMap = Map.of("rno",111L);
+        Map<String, Long> resultMap = new HashMap<>();
+        Long rno = replyService.register(replyDTO);
+
+        resultMap.put("rno", rno);
+
         return resultMap;
     }
+
+
+    // 특정  게시물의 댓글 목록 보기...
+    // URI -> '/replies/list/{bno}' , 방식 -> GET
+    // @PathVariable 어노테이션 사용!
+    @Operation(summary = "Replies of Board로 GET방식으로 특정 게시물 댓글 목록 처리...")
+    @GetMapping(value = "/list/{bno}")
+    public PageResponseDTO<ReplyDTO> getList(
+            @PathVariable("bno") Long bno,
+            PageRequestDTO pageRequestDTO) {
+
+        PageResponseDTO<ReplyDTO> responseDTO = replyService.getListOfBoard(bno,pageRequestDTO);
+
+        return responseDTO;
+    }
+
 
 
 }
--- src/main/java/org/zerock/b01/controller/advice/CustomRestAdvice.java
+++ src/main/java/org/zerock/b01/controller/advice/CustomRestAdvice.java
@@ -1,6 +1,7 @@
 package org.zerock.b01.controller.advice;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -33,6 +34,19 @@ public class CustomRestAdvice {
         return ResponseEntity.badRequest().body(errorMap);
     }
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    public ResponseEntity<Map<String, String>> handlerFKException(Exception e) {
+        log.error(e);
+
+        Map<String, String> errorMap = new HashMap<>();
+
+        errorMap.put("time", ""+System.currentTimeMillis());
+        errorMap.put("msg","constraint fails");
+
+        return ResponseEntity.badRequest().body(errorMap);
+
+    }
 
 
 }
